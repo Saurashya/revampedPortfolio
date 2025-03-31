378d92177f4e83394838eff9f44d535b1e4682fb
saurashya <saurashya1@gmail.com>
2025-03-31 21:23:51 +0545

    Fixed imports

--- data/gallery.ts
+++ data/gallery.ts
@@ -1,7 +1,7 @@
-import saurashya1 from "/public/assets/images/saurashya1.png";
-import saurashya2 from "/public/assets/images/saurashya2.png";
-import saurashya3 from "/public/assets/images/saurashya3.png"; 
-import saurashya4 from "/public/assets/images/saurashya4.png"; 
+import saurashya1 from "@public/assets/images/saurashya1.png";
+import saurashya2 from "@public/assets/images/saurashya2.png";
+import saurashya3 from "@public/assets/images/saurashya3.png"; 
+import saurashya4 from "@public/assets/images/saurashya4.png"; 
 
 export const galleryImages =[
     {
